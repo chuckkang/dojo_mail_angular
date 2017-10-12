cd51cf4c38df0c3fd9d181e213a4eab847fe524f
Chuck Kang <chuckkang@gmail.com>
2017-10-12 16:47:52 -0700

    dojo mail angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app';
+title = 'Dojo Mail';
+emaildata = [
+	new DojoMail ("bill@gates.com", false, "New Windows", "X1 will launch"), 
+	new DojoMail("ada@lovelace.com", true, "Programming", "Enchantress	"), 
+	new DojoMail("john@carmac.com", false, "Updated Algo", "New Algo"), 
+	new DojoMail("gabe@newel.com", true, "HL3", "Angular assignment"), 
+	new DojoMail("chuck@asdf.com", false, "Start on Angular", "Angular"), 
+];
+
+}
+class DojoMail {
+	email: string;
+	importance: boolean;
+	subject: string;
+	content: string;
+	constructor(email: string, importance: boolean, subject: string, content: string) {
+		this.email = email;
+		this.importance = importance;
+		this.subject = subject;
+		this.content = content;
+	}
 }
